b09ec354656a4e94c357dd73879dbffb003995a3
artross <artross.bodhi@gmail.com>
2016-11-12 23:47:37 +0300

    QuestionsController ready

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,24 @@
 class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.create(question_params)
+    @question.persisted? ? (redirect_to @question) : (render :new)
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,4 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  describe 'GET #index' do
+    let(:questions) { create_list(:question, 2) }
+    before { get :index }
+
+    it 'assigns an array' do
+      expect(assigns(:questions)).to match_array(questions)
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    let(:question) { create(:question) }
+    before { get :show, params: {id: question} }
+
+    it 'assings question from params' do
+      expect(assigns(:question)).to eq(question)
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'creates a new Question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid object' do
+      it 'persists an object' do
+        expect { post :create, params: {question: attributes_for(:question)} }.to change(Question, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, params: {question: attributes_for(:question)}
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context 'invalid object' do
+      it "doesn't persist an object" do
+        expect { post :create, params: {question: attributes_for(:invalid_question)} }.not_to change(Question, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, params: {question: attributes_for(:invalid_question)}
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
