56282c75a2e1c83f8be6835465e5151429d0006f
artross <artross.bodhi@gmail.com>
2016-12-04 22:41:22 +0300

    small fix: update -> update! in transaction

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -8,7 +8,7 @@ class Answer < ApplicationRecord
     question = self.question
     Answer.transaction do
       question.answers.update_all(best_answer: false)
-      self.update(best_answer: true)
+      self.update!(best_answer: true)
     end
   end
 end
