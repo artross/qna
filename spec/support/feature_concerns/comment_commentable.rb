dda8ed2e60684d8b2552d775160733ad677493a8
artross <artross.bodhi@gmail.com>
2017-02-06 17:25:56 +0300

    comment questions ready

--- spec/support/feature_concerns/comment_commentable.rb
+++ spec/support/feature_concerns/comment_commentable.rb
@@ -4,18 +4,18 @@ shared_examples_for 'comment_commentable' do
   scenario "User adds a comment", js: true do
     within(@region) do
       click_on "Add comment"
-      expect(page).to have_content "Submit"       # form appeared
+      expect(page).to have_content "Your comment"       # form appeared
       expect(page).not_to have_link "Add comment"
 
       fill_in "Your comment", with: "New comment"
       click_on "Submit"
 
-      expect(page).not_to have_content "Submit"   # form became hidden
+      expect(page).not_to have_content "Your comment"   # form became hidden
       expect(page).to have_link "Add comment"
 
       expect(page).to have_content "New comment"
-      expect(page).to have_content "Comment successfully added"
     end
+    expect(page).to have_content "Comment successfully added"
   end
 
   scenario "User can't add blank comment", js: true do
@@ -23,10 +23,9 @@ shared_examples_for 'comment_commentable' do
       click_on "Add comment"
       click_on "Submit"
 
-      expect(page).to have_content "Submit"       # form is still there
-      expect(page).not_to have_link "Add comment" # link is still hidden
-
-      expect(page).to have_content "Can't add such comment"
+      expect(page).to have_content "Your comment"       # form is still there
+      expect(page).not_to have_link "Add comment"       # link is still hidden
     end
+    expect(page).to have_content "Unable to add such comment"
   end
 end
